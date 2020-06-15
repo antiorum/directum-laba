057d05c8949148ca3575ef25b521870ab04ab3e0
antiorum <antiorum@inbox.ru>
2020-06-15 13:00:53 +0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace directum_laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
